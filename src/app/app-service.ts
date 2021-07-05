5a2beb071b17a00b7f9a1e4d6edb3fcd25c15a77
Abhishek Yadav <yadav.abhishek@siemens.com>
2021-07-05 16:30:28 +0530

    load balancer ip

--- src/app/app-service.ts
+++ src/app/app-service.ts
@@ -5,6 +5,7 @@ import { Observable, throwError } from 'rxjs';
 @Injectable()
 export class StakaterHelloService {
  baseURL: string = "http://localhost:8080/";
+ //baseURL: string = "http:/10.168.0.102/"; # Specifiy load balancer ip here
 
   constructor(private http: HttpClient) {
   }
